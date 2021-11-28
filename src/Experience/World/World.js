077e8f99f0d424ed0f16dc38fe6307a5bab54b2b
rqphy <raphael.ferreira@hetic.net>
2021-11-28 22:53:59 +0100

    add world

---
+++ src/Experience/World/World.js
@@ -0,0 +1,20 @@
+import Experience from '../Experience'
+import Environment from './Environment'
+
+export default class World
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+        // Wait for resources
+        this.resources.on('ready', () =>
+        {
+            // Setup
+            this.environment = new Environment()
+        })
+    }
+    
+}
