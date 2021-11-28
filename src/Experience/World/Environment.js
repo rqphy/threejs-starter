5f6d1523f1a1f39512fc93ac9264e0847ea1d40e
rqphy <raphael.ferreira@hetic.net>
2021-11-28 23:20:41 +0100

    syntax correction DirectionalLight color

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -17,12 +17,11 @@ export default class Environment
         }
 
         this.setSunlight()
-        console.log('setsunlight')
     }
 
     setSunlight()
     {
-        this.sunLight = new THREE.DirectionalLight('#ffffff, 4')
+        this.sunLight = new THREE.DirectionalLight('#ffffff', 4)
         this.sunLight.castShadow = true
         this.sunLight.shadow.camera.far = 15
         this.sunLight.shadow.mapSize.set(1024, 1024)
